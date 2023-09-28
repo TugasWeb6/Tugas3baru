22c61628f7927371bb5d2cc85547bcc020447fea
TugasWeb6 <144772458+TugasWeb6@users.noreply.github.com>
2023-09-28 13:24:29 +0700

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,1 +1,53 @@
+<!DOCTYPE html>
 
+<html>
+
+<head>
+
+<title>Form Login Multi User www.malasngoding.com</title>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body>
+
+<h1>Membuat Login Multi User Dengan PHP dan MySQLi<br/>www.panduancode.com</h1>
+
+<?php 
+
+if(isset($_GET['pesan'])){
+
+if($_GET['pesan']=="gagal"){
+
+echo "<div class='alert'>Username atau Password tidak sesuai !</div>";
+
+}
+
+}
+
+?>
+
+<div class="kotak_login">
+
+<p class="tulisan_login">Silahkan login</p>
+
+<form action="cek_login.php" method="post">
+
+<label>Username</label>
+
+<input type="text" name="username" class="form_login" placeholder="Username .." required="required">
+
+<label>Password</label>
+
+<input type="password" name="password" class="form_login" placeholder="Password .." required="required">
+
+<input type="submit" class="tombol_login" value="LOGIN">
+
+</form>
+
+</div>
+
+</body>
+
+</html>
