f2db3f84b85ebc5a747cc07f45253968bb75fed0
TugasWeb6 <144772458+TugasWeb6@users.noreply.github.com>
2023-09-28 13:19:38 +0700

    Update Halaman_pegawai.php

--- Halaman_pegawai.php
+++ Halaman_pegawai.php
@@ -1,1 +1,18 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+<title>Halaman Untuk Pegawai</title>
+</head>
+<body>
+<?php 
+session_start();
+// Untuk mengcek apakah yang mengakses halaman ini sudah login atau belum
+if($_SESSION['level']==""){
+header("location:index.php?pesan=gagal");
+}
+?>
+<h1>Halaman Pegawai</h1>
+<p>Halo <b><?php echo $_SESSION['username']; ?></b> Anda telah berhasil login sebagai <b><?php echo $_SESSION['level']; ?></b>.</p>
+<a href="logout.php">LOGOUT</a>
+</body>
+</html>
